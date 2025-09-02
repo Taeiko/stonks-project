e504efba91ab013f06d8d938a24e65d0e7c55b1f
Is an English Major <Najeebihesa@gmail.com>
2025-09-02 14:24:09 +0300

    working on the READ part of the CRUD

--- stonks_app/urls.py
+++ stonks_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 # these are the urls, where the views pages live, the name is like a key to take us there.
 urlpatterns = [
     path('',views.welcome, name='welcome' ),
-    path('stocks/create/', views.StockCreateView.as_view(), name='stock_create')
+    path('stocks/create/', views.StockCreateView.as_view(), name='stock_create'),
+    path('stocks/portfolio/', views.StockListView.as_view(), name ='stock_list'),
 ]
--- stonks_app/views.py
+++ stonks_app/views.py
@@ -6,7 +6,7 @@ def welcome(request):
     return render(request, 'welcome.html')
 
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .models import Stock
 from .forms import StockForm
 
@@ -14,3 +14,8 @@ class StockCreateView(CreateView):
     model = Stock
     form_class = StockForm
     template_name = 'stocks/stock-form.html'
+
+class StockListView(ListView):
+    model = Stock
+    template_name = 'stocks/stock-list.html'
+    context_object_name= 'stock'
