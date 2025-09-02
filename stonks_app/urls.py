dbc141f8f5f2a1f291bb4374215474e527f5fd15
Is an English Major <Najeebihesa@gmail.com>
2025-09-02 10:54:46 +0300

    testing welcome page routing because screw django routes

--- stonks_app/urls.py
+++ stonks_app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
+# these are the urls, where the views pages live, the name is like a key to take us there.
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('stonks_app.urls')),
+    path('',views.welcome, name='welcome' )
 ]
--- stonks_app/views.py
+++ stonks_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+# these are the pages that get displayed when the user types the url
+def welcome(request):
+    return render(request, 'welcome.html')
